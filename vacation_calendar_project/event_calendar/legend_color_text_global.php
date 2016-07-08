dd541cf075e8bea41d7488fa9e13f9741f89ab4d
alex-cres <alexrealinho@gmail.com>
2016-07-08 15:26:21 +0100

    Revert "Revert "Changed names and changed table theme""
    This reverts commit a93a6037f24ee5fff3f67abbd4380520cea6caec.

--- vacation_calendar_project/event_calendar/legend_color_text_global.php
+++ vacation_calendar_project/event_calendar/legend_color_text_global.php
@@ -3,7 +3,7 @@
 if (module_exists("event_calendar_colors")) {
     $query_one      = db_select('taxonomy_vocabulary', 'ttd')->fields('ttd')->condition('machine_name', 'event_calendar_status', '=');
     $result_one     = $query_one->execute();
-    $term_color_div="<table class='full'>";
+    $term_color_div="<div class='full'>";
 	$w=1;
     foreach ($result_one as $vid) {
 	
@@ -11,7 +11,7 @@ if (module_exists("event_calendar_colors")) {
         $result = $query->execute();
         foreach ($result as $fetch) {
             if($w%2==1){
-			 $term_color_div.="<tr>";
+			 $term_color_div.="<div>";
 			}
 			$selector                = 'colors_taxonomy_term_' . $fetch->tid;
             $color                   = db_select('event_colors', 'ec')->fields('ec', array(
@@ -19,22 +19,22 @@ if (module_exists("event_calendar_colors")) {
             ))->condition('selector', $selector)->execute()->fetchField();
             $ser_color               = unserialize($color);
             $term_color[$fetch->tid] = $ser_color['background'];
-            $term_color_div .= "<td><div style=' display:inline-block;height:20px;width:20px;background-color:" . $ser_color['background'] . "'>&nbsp;&nbsp;&nbsp;&nbsp;</div> - " . $fetch->name;
+            $term_color_div .= "<div><div style=' display:inline-block;height:20px;width:20px;background-color:" . $ser_color['background'] . "'>&nbsp;&nbsp;&nbsp;&nbsp;</div> - " . $fetch->name;
 			if($fetch->tid==_get_term_from_name3("approved", "event_calendar_status")){
 			 $term_color_div .=": "._measure_consumed_days2($user->uid, 3);		
 			} elseif($fetch->tid==_get_term_from_name3("submitted", "event_calendar_status")){
 			 $term_color_div .=": "._measure_consumed_days2($user->uid, 2);		
-			}elseif($fetch->tid==_get_term_from_name3("pending", "event_calendar_status")){
+			}elseif($fetch->tid==_get_term_from_name3("draft", "event_calendar_status")){
 			 $term_color_div .=": "._measure_consumed_days2($user->uid, 4);		
 			}  
-			$term_color_div .= "</td>";
+			$term_color_div .= "</div>";
 			 if($w%2==0){
-			 $term_color_div.="</tr>";
+			 $term_color_div.="</div>";
 			}
         $w++;
 		}
     }
-    $term_color_div .= "</table>";
+    $term_color_div .= "</div>";
     echo $term_color_div;
 }
 ?>
